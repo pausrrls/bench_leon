a3deb12d3cec18de437b61612a100c7806392600
Char-Al <charles.van.goethem@gmail.com>
2016-08-05 12:08:54 +0200

    For testing

--- compare_leon.pl
+++ compare_leon.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -w
 # Author : Charles VAN GOETHEM
 
-
 # Perl general libs
 use warnings;
 use strict;
@@ -15,6 +14,8 @@ use Cwd;
 use v5.14;
 no warnings 'experimental::smartmatch';
 
+use File::stat;	
+
 
 ##########################################################################################
 ##########################################################################################
@@ -22,6 +23,7 @@ no warnings 'experimental::smartmatch';
 
 my $path_leon = "/Users/adminbioinfo/Documents/Leon/";
 my $pwd = getcwd();
+
 
 ##########################################################################################
 ##########################################################################################
@@ -31,7 +33,7 @@ my @files;
 my @directories;
 
 # Optional arguments
-my $output = $pwd;
+my $output = $pwd."/";
 
 # General arguments
 my $man 		= 0;
@@ -62,30 +64,53 @@ pod2usage("Error : need file(s) and/or directory(ies).") unless (@files or @dire
 
 # Check the files
 foreach my $file (@files) {
-	pod2usage("Error : $file is not a plain text or did not exist.") unless (-f $file);
+	pod2usage("Error : $file is not a plain text or not exist.") unless (-f $file);
 	
 	# Check if file is a fastq or fastq.gz
-	my ($name, $dir, $ext) = fileparse($file,qw(.fastq .fastq.gz));
-	pod2usage("Error : $file is not a fastq or a fastq.gz.") unless ($ext);
+	#	my ($name, $dir, $ext) = fileparse($file,qw(.fastq .fastq.gz));
+	my ($name, $dir, $ext) = fileparse($file,qw(.fastq));
+	pod2usage("Error : $file is not a fastq or a fastq.gz. (only fastq until now)") unless ($ext);
 }
 
 # Check directories and get fastq and fastq.gz
 foreach my $directory (@directories) {
-	pod2usage("Error : $directory is not a directory or did not exist.") unless (-d $directory);
+	pod2usage("Error : $directory is not a directory or not exist.") unless (-d $directory);
 	
 	# Complete the list of files with the fastq and fastq.gz in the repertory
-	push(@files, glob "$directory/*.{fastq,fastq.gz}");
+	#push(@files, glob "$directory/*.{fastq,fastq.gz}");
+	push(@files, glob "$directory/*.fastq");
 }
 
 # print Dumper @files;
 
+my $filename = $output."/report.tab";
+open(REPORT , ">$filename") or die "Could not open file '$filename' $!";
+
+######## Header file (on the same line)
+#	File_ID	Size_fastQ
+#	Time_comp_leon_lossy	Size_comp_leon_lossy
+#	Time_comp_leon_lossless	Size_comp_leon_lossless
+#	Time_comp_gzip1	Size_gzip1
+#	Time_comp_gzip6	Size_gzip6
+#	Time_comp_gzip9	Size_gzip9
+#	Time_uncomp_leon_lossy	Time_uncomp_leon_lossless
+#	Time_uncomp_gzip1	Time_uncomp_gzip6	Time_uncomp_gzip9
+print OUT "File_ID\tSize_fastQ\t";
+print OUT "Time_comp_leon_lossy\tSize_comp_leon_lossy\t";
+print OUT "Time_comp_leon_lossless\tSize_comp_leon_lossless\t";
+print OUT "Time_comp_gzip1\tSize_gzip1\t";
+print OUT "Time_comp_gzip6\tSize_gzip6\t";
+print OUT "Time_comp_gzip9\tSize_gzip9\t";
+print OUT "Time_uncomp_leon_lossy\tTime_uncomp_leon_lossless\t";
+print OUT "Time_uncomp_gzip1\tTime_uncomp_gzip6\tTime_uncomp_gzip9\n";
+
+
 ##########################################################################################
 ##########################################################################################
 
-my $timer_S;
-my $timer_E;
-###### Step 
 
+my $i=1;
+my $line ="";
 foreach my $file (@files) {
 	my ($name, $dir, $ext) = fileparse($file,qw(.fastq .fastq.gz));
 	
@@ -99,27 +124,112 @@ foreach my $file (@files) {
 		#}
 	}
 	
-	system("ln -s ".$dir.$name.".fastq ".$out.$name.".lossy-leon.fastq");
-	system("ln -s ".$dir.$name.".fastq ".$out.$name.".lossless-leon.fastq");
+	my $size_origin = stat($file)->size;
+	$line = "File_".$i."\t".$size_origin."\t";
 	
-	$timer_S = time();
-	system($path_leon." -f ".$dir.$name.".fastq\n -c");
-	$timer_E = time();
-	my $comp_leon_lossy_time = $timer_E - $timer_S;
+	##################################
+	######### Compressions
 	
-	$timer_S = time();
-	system($path_leon." -f ".$dir.$name.".fastq\n -c -lossless");
-	$timer_E = time();
-	my $comp_leon_lossless_time = $timer_E - $timer_S;
+	##################################
+	##### LEON
+	## lossy (best)
+	## lossless (depreciated)
 	
+	my $file_leon_lossy = $output.$name.".lossy-leon.fastq";
+	my $file_leon_lossless = $output.$name.".lossless-leon.fastq";
+	
+	system("ln -s ".$dir.$name.".fastq ".$file_leon_lossy);
+	system("ln -s ".$dir.$name.".fastq ".$file_leon_lossless);
+	
+	my $time_comp_leon_lossy = timer_cmd_bash($path_leon." -f ".$file_leon_lossy." -c");
+	my $time_comp_leon_lossless = timer_cmd_bash($path_leon." -f ".$file_leon_lossless." -c -lossless");
+	
+	my $size_lossy = size_for_leo_files($file_leon_lossy);
+	my $size_lossless = size_for_leo_files($file_leon_lossless);
+	
+	$line .= $time_comp_leon_lossy."\t".$size_lossy."\t";
+	$line .= $time_comp_leon_lossless."\t".$size_lossless."\t";
+	
+	##################################
+	##### Gzip
+	## level 1 (fastest)
+	## level 6 (default)
+	## level 9 (best)
+	
+	my $file_gzip1 = $output.$name."_level-1.fastq.gz";
+	my $file_gzip6 = $output.$name."_level-6.fastq.gz";
+	my $file_gzip9 = $output.$name."_level-9.fastq.gz";
+	
+	my $time_comp_gzip1 = timer_cmd_bash("gzip -ck1 ".$file." > ".$file_gzip1);
+	my $time_comp_gzip6 = timer_cmd_bash("gzip -ck6 ".$file." > ".$file_gzip6);
+	my $time_comp_gzip9 = timer_cmd_bash("gzip -ck9 ".$file." > ".$file_gzip9);
+	
+	my $size_gzip1 = stat($file_gzip1)->size;
+	my $size_gzip6 = stat($file_gzip6)->size;
+	my $size_gzip9 = stat($file_gzip9)->size;
+	
+	$line .= $time_comp_gzip1."\t".$size_gzip1."\t";
+	$line .= $time_comp_gzip6."\t".$size_gzip6."\t";
+	$line .= $time_comp_gzip9."\t".$size_gzip9."\t";
+	
+	##################################
+	######### Uncompresses
+	
+	##################################
+	##### LEON
+	
+	my $time_uncomp_leon_lossy = timer_cmd_bash($path_leon." -f ".$file_leon_lossy.".leon -d");
+	my $time_uncomp_leon_lossless = timer_cmd_bash($path_leon." -f ".$file_leon_lossless.".leon -d");
+	
+	$line .= $time_uncomp_leon_lossy."\t".$time_uncomp_leon_lossless."\t";
+	
+	##################################
+	##### Gzip
+	
+	my $time_uncomp_gzip1 = timer_cmd_bash("gunzip -ck ".$file_gzip1);
+	my $time_uncomp_gzip6 = timer_cmd_bash("gunzip -ck ".$file_gzip6);
+	my $time_uncomp_gzip9 = timer_cmd_bash("gunzip -ck ".$file_gzip9);
+	
+	$line .= $time_uncomp_gzip1."\t".$time_uncomp_gzip6."\t".$time_uncomp_gzip9."\n";
+	
+	##################################
+	######### Write .tab file
+	
+	print OUT $line;
+	
+	$i++;
 }
 
 ##########################################################################################
 ##########################################################################################
 
 
-sub benchFastq {
+sub timer_cmd_bash {
+	my $cmd = shift(@_);
 	
+	my $timer_S;
+	my $timer_E;
+	my $diff;
+	
+	$timer_S = time();
+	system($cmd);
+	#print $cmd."\n";
+	$timer_E = time();
+	
+	$diff = $timer_E - $timer_S;
+	
+	return $diff;
+}
+
+sub size_for_leo_files {
+	my $file = shift(@_);
+
+	my $stat_leon = stat($file.".leon");
+	my $stat_qual = stat($file.".qual");
+	
+	my $size_total = ($stat_leon->size) + ($stat_qual->size);
+	
+	return $size_total;
 }
 
 
